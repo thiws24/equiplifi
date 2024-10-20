13a8097cc7316b95e57c0b6e24049ad038a2184a
RR <raluca97rr@gmail.com>
2024-10-20 10:29:42 +0200

    Anpassung Tests
    App Test abgepasst und Tests für die neue Komponente geschrieben

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App Tests', () => {
+  test('renders the AG Grid table', async () => {
+    const { container } = render(<App/>)
+    expect(container.querySelector('.ag-root')).not.toBeNull()
+  });
+
+  test('renders 10 elements in the table body', async () => {
+    const { container } = render(<App/>)
+    await waitFor(async() => {
+      const elements = container?.querySelector('.ag-body')?.querySelectorAll('[role="row"]')
+      expect(elements?.length).toBe(10)
+    }, {
+      timeout: 3000,
+    })
+  })
+})
