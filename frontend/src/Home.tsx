9a5de9ea236df27ce388e08d22c58544a89acfcf
HeiligeMeier <steffheili846@t-online.de>
2024-10-26 22:14:20 +0200

    Renamed App file to Home

---
+++ frontend/src/Home.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-quartz.css";
+import { ColDef } from "ag-grid-community"
+import { InventoryItemProps } from "./interfaces/InventoryItemProps";
+import { TableGalleryView } from "./components/TableGalleryView";
+
+function Home() {
+    const [inventoryItems, setInventoryItems] = React.useState<InventoryItemProps[]>([])
+
+    const colDefs: ColDef<InventoryItemProps, any>[] = [
+        { field: "id", flex: 1, filter: 'agSetColumnFilter' },
+        { field: "name", flex: 1, filter: 'agSetColumnFilter' },
+        { field: "icon", flex: 1, filter: 'agSetColumnFilter' },
+        { field: "photo", flex: 1 },
+        { field: "urn", flex: 1, filter: 'agSetColumnFilter' }
+    ]
+
+    const [loading, setLoading] = React.useState(true);
+
+    async function fetchInventoryItems() {
+        try {
+            const response = await fetch(`${process.env.REACT_APP_II_SERVICE_HOST}/api/inventoryitems`)
+            if (response.ok) {
+                const data = await response.json();
+                setInventoryItems(data);
+            }
+        } catch (e) {
+            console.log(e)
+        }
+
+        setLoading(false);
+    }
+
+
+    React.useEffect(() => {
+        void fetchInventoryItems();
+    }, [])
+
+    return (
+        <div className="m-10">
+            <main className="main">
+                <TableGalleryView
+                    data={inventoryItems}
+                    colDefs={colDefs}
+                    loading={loading}
+                />
+            </main>
+        </div>
+    );
+
+}
+
+export default Home;
