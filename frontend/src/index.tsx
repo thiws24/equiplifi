4e526dba437358879dd54b7ef173df38844da008
HeiligeMeier <steffheili846@t-online.de>
2024-10-26 22:15:25 +0200

    minor changes in root.render

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,15 +4,16 @@ import './index.css';
 import './globals.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(  document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
