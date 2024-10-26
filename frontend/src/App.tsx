1d318326298603d8736057d3f3e1d4458246415f
HeiligeMeier <steffheili846@t-online.de>
2024-10-26 22:12:17 +0200

    Changed App to Routing file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,54 +1,16 @@
-import React from 'react';
-import "ag-grid-community/styles/ag-grid.css";
-import "ag-grid-community/styles/ag-theme-quartz.css";
-import { ColDef } from "ag-grid-community"
-import { InventoryItemProps } from "./interfaces/InventoryItemProps";
-import { TableGalleryView } from "./components/TableGalleryView";
+import { Routes, Route } from 'react-router-dom';
+import Detail from "./Detail";
+import Home from "./Home";
 
-function App() {
-  const [inventoryItems, setInventoryItems] = React.useState<InventoryItemProps[]>([])
-
-  const colDefs: ColDef<InventoryItemProps, any>[] = [
-    { field: "id", flex: 1, filter: 'agSetColumnFilter' },
-    { field: "name", flex: 1, filter: 'agSetColumnFilter' },
-    { field: "icon", flex: 1, filter: 'agSetColumnFilter' },
-    { field: "photo", flex: 1 },
-    { field: "urn", flex: 1, filter: 'agSetColumnFilter' }
-  ]
-
-  const [loading, setLoading] = React.useState(true);
-
-  async function fetchInventoryItems() {
-    try {
-      const response = await fetch(`${process.env.REACT_APP_II_SERVICE_HOST}/api/inventoryitems`)
-      if (response.ok) {
-        const data = await response.json();
-        setInventoryItems(data);
-      }
-    } catch (e) {
-      console.log(e)
-    }
-
-    setLoading(false);
-  }
-
-
-  React.useEffect(() => {
-    void fetchInventoryItems();
-  }, [])
-
+const App = () => {
   return (
-    <div className="m-10">
-      <main className="main">
-        <TableGalleryView
-          data={inventoryItems}
-          colDefs={colDefs}
-          loading={loading}
-        />
-      </main>
-    </div>
+      <>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/inventory-item/:id" element={<Detail />} />
+          </Routes>
+      </>
   );
-
-}
+};
 
 export default App;
