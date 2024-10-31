4b144884416ffa43072053acd6c0b98ee26680d0
nadri23 <nad.riedl@web.de>
2024-10-31 17:17:35 +0100

    update
    - created Reservation entity
    - created ReservationRepository
    
    Co-Authored-By: mfaerber1198 <184264038+mfaerber1198@users.noreply.github.com>
    Co-Authored-By: zom5583 <103882767+zom5583@users.noreply.github.com>

--- reservation_service/src/test/java/de/equipli/ReservationResourceTest.java
+++ reservation_service/src/test/java/de/equipli/ReservationResourceTest.java
@@ -7,14 +7,14 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-class GreetingResourceTest {
-    @Test
+class ReservationResourceTest {
+/*    @Test
     void testHelloEndpoint() {
         given()
           .when().get("/hello")
           .then()
              .statusCode(200)
              .body(is("Hello from Quarkus REST"));
-    }
+    }*/
 
 }
