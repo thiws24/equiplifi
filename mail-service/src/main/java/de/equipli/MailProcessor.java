835f880e8ee655f483746299580b2edd8f708f34
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2024-11-05 18:28:47 +0100

    ✨implemented basic smtp service

---
+++ mail-service/src/main/java/de/equipli/MailProcessor.java
@@ -0,0 +1,20 @@
+package de.equipli;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+@ApplicationScoped
+public class MailProcessor implements Processor {
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        MailDTO mailDTO = exchange.getIn().getBody(MailDTO.class);
+        exchange.getIn().setHeader("Subject", mailDTO.getSubject());
+        exchange.getIn().setHeader("To", mailDTO.getTo());
+        exchange.getIn().setHeader("From", mailDTO.getFrom());
+        exchange.getIn().setHeader("Content-Type", "text/html");
+        exchange.getIn().setBody(mailDTO.getBody());
+        System.out.println("Body: " + mailDTO.getBody());
+        
+    }
+}
---
+++ mail-service/src/main/java/de/equipli/Route.java
@@ -0,0 +1,36 @@
+package de.equipli;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.apache.camel.builder.RouteBuilder;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@ApplicationScoped
+public class Route extends RouteBuilder {
+    
+    @Inject
+    MailProcessor mailProcessor;
+
+    @ConfigProperty(name = "smtp.host")
+    String smtpHost;
+    
+    @ConfigProperty(name = "smtp.port")
+    String smtpPort;
+    
+    @Override
+    public void configure() throws Exception {
+        rest().get("/hello")
+                .to("direct:hello")
+                .post("sendMail")
+                .type(MailDTO.class)
+                .to("direct:sendmail");
+        
+        from("direct:hello")
+                .transform().constant("Hello World");
+        
+        from("direct:sendmail")
+                .unmarshal().json(MailDTO.class)
+                .process(mailProcessor)
+                .to("smtp://" + smtpHost + ":" + smtpPort );
+    }
+}
