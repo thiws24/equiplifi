7193e2c493f14dbca81d4c4007fb37c3a8295256
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2024-11-10 16:24:57 +0100

    ✨ added first mail Route with html Template

--- mail-service/src/main/java/de/equipli/Routes/MailRoute.java
+++ mail-service/src/main/java/de/equipli/Routes/MailRoute.java
@@ -1,6 +1,8 @@
 package de.equipli.Routes;
 
 import de.equipli.DTOs.MailDTO;
+import de.equipli.GenericResponse;
+import de.equipli.Processors.CollectMailProcessor;
 import de.equipli.Processors.ReturnMailProcessor;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -8,38 +10,46 @@ import org.apache.camel.builder.RouteBuilder;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
-public class Route extends RouteBuilder {
-    
-    @Inject
-    ReturnMailProcessor mailProcessor;
+public class MailRoute extends RouteBuilder {
 
-    @ConfigProperty(name = "smtp.host")
+    @Inject
+    CollectMailProcessor collectMailProcessor;
+
+    @Inject
+    ReturnMailProcessor returnMailProcessor;
+
+    @ConfigProperty(name = "CONFIG.SMTP.HOST", defaultValue = "localhost")
     String smtpHost;
     
-    @ConfigProperty(name = "smtp.port")
+    @ConfigProperty(name = "CONFIG.SMTP.PORT", defaultValue = "2525")
     String smtpPort;
-    
+
     @Override
     public void configure() throws Exception {
         rest()
                 .post("sendCollectionMail")
                 .type(MailDTO.class)
                 .to("direct:sendCollectionMail")
-                
+
                 .post("sendReturnMail")
                 .type(MailDTO.class)
                 .to("direct:sendReturnMail");
-        
 
-        
+
         from("direct:sendCollectionMail")
                 .unmarshal().json(MailDTO.class)
-                .process(mailProcessor)
-                .to("smtp://" + smtpHost + ":" + smtpPort );
+                .process(collectMailProcessor)
+                .to("smtp://" + smtpHost + ":" + smtpPort)
+                .process(
+                        exchange -> exchange.getIn().setBody(
+                                new GenericResponse("Mail sent successfully")
+                        )
+                )
+                .marshal().json();
 
         from("direct:sendReturnMail")
                 .unmarshal().json(MailDTO.class)
-                .process(mailProcessor)
-                .to("smtp://" + smtpHost + ":" + smtpPort );
+                .process(returnMailProcessor)
+                .to("smtp://" + smtpHost + ":" + smtpPort);
     }
 }
