86386a29384f7883823e75f053d2927ef71b9d5a
janniknu <jannik.nuesslein@gmx.de>
2024-11-12 21:47:05 +0100

    backend ci workflow test

--- backend/inventoryservice/src/main/java/de/equipli/InventoryItemResource.java
+++ backend/inventoryservice/src/main/java/de/equipli/InventoryItemResource.java
@@ -81,7 +81,7 @@ public class InventoryItemResource {
         if (existingItem == null) {
             throw new WebApplicationException("Inventory item with id '" + id + "' not found", 404);
         }
-        System.out.println("test");
+        System.out.println("tes");
         inventoryItemRepository.delete(existingItem);
     }
 }
