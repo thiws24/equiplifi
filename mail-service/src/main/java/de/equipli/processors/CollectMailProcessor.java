db8933e196d1fc7a89a1cb986ab408c3e67d9586
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2024-11-18 21:26:15 +0100

    🏗️ refactored mail service to use separate DTOs

--- mail-service/src/main/java/de/equipli/processors/CollectMailProcessor.java
+++ mail-service/src/main/java/de/equipli/processors/CollectMailProcessor.java
@@ -1,40 +1,39 @@
 package de.equipli.processors;
 
-import de.equipli.dto.MailDTO;
+import de.equipli.dto.CollectMailDto;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
-
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import org.apache.camel.Processor;;
 
 @ApplicationScoped
 public class CollectMailProcessor implements Processor {
 
 
     @Inject
-    Template pickupReminder;
+    Template collectmail;
 
     @Override
     public void process(Exchange exchange) throws Exception {
-        MailDTO mailDTO = exchange.getIn().getBody(MailDTO.class);
+        CollectMailDto collectMailDto = exchange.getIn().getBody(CollectMailDto.class);
+
+        exchange.setProperty("to", collectMailDto.getReceiverMail());
 
         // Render the template with Qute
-        TemplateInstance templateInstance = pickupReminder
-                .data("item", mailDTO.getItem())
-                .data("collectionDate", mailDTO.getCollectionDate())
-                .data("returnDate", mailDTO.getReturnDate())
-                .data("pickupLocation", mailDTO.getPickupLocation())
-                .data("receiver", mailDTO.getTo());
+        TemplateInstance templateInstance = collectmail
+                .data("name", collectMailDto.getName())
+                .data("item", collectMailDto.getItem())
+                .data("collectionDate", collectMailDto.getCollectionDate())
+                .data("returnDate", collectMailDto.getReturnDate())
+                .data("pickupLocation", collectMailDto.getPickupLocation());
 
         String htmlTemplate = templateInstance.render();
 
         // create mail
         exchange.getIn().setHeader("Subject", "Abholerinnerung | Equipli");
-        exchange.getIn().setHeader("To", mailDTO.getTo());
+        exchange.getIn().setHeader("To", collectMailDto.getReceiverMail());
         exchange.getIn().setHeader("From", "info@equipli.de");
         exchange.getIn().setHeader("Content-Type", "text/html");
         exchange.getIn().setBody(htmlTemplate);
