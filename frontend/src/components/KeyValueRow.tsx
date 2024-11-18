2cbdc4f45a1b2b2064f9948125649301cb1d44a9
HeiligeMeier <steffheili846@t-online.de>
2024-11-18 20:55:38 +0100

    Code Refactoring

--- frontend/src/components/KeyValueRow.tsx
+++ frontend/src/components/KeyValueRow.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 
 interface Props {
-    label: string
-    value: any
+    label: string | React.ReactNode
+    children: React.ReactNode
 }
 
-export const KeyValueRow: React.FC<Props> = ({ label, value}) => {
+export const KeyValueRow: React.FC<Props> = ({ label, children }) => {
     return (
         <div className="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-0">
             <dt className="text-sm font-medium leading-6 text-customBlack">{label}</dt>
-            <dd className="mt-1 text-sm leading-6 text-customBlack sm:col-span-2 sm:mt-0">{value}</dd>
+            <dd className="mt-1 text-sm leading-6 text-customBlack sm:col-span-2 sm:mt-0">{children}</dd>
         </div>
     );
-};
+}
