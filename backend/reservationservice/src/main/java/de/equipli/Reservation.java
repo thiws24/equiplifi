f2710c97c25b3228548ea714acacd13fa0daa261
Marvin <kern.marvin@outlook.de>
2024-11-21 13:10:38 +0100

    Updated ReservationService to match database model

--- backend/reservationservice/src/main/java/de/equipli/Reservation.java
+++ backend/reservationservice/src/main/java/de/equipli/Reservation.java
@@ -7,10 +7,12 @@ import java.time.LocalDate;
 
 @Entity
 public class Reservation extends PanacheEntity {
-    
+
+    public Long itemId;
+    public Long userId;
     public LocalDate startDate;
     public LocalDate endDate;
-    public Long itemId;
+    public String reservationStatus;
 
     public Long getId() {
         return id;
@@ -18,6 +20,22 @@ public class Reservation extends PanacheEntity {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
     }
 
     public LocalDate getStartDate() {
@@ -36,12 +54,12 @@ public class Reservation extends PanacheEntity {
         this.endDate = endDate;
     }
 
-    public Long getItemId() {
-        return itemId;
+    public String getReservationStatus() {
+        return reservationStatus;
     }
 
-    public void setItemId(Long itemId) {
-        this.itemId = itemId;
+    public void setReservationStatus(String reservationStatus) {
+        this.reservationStatus = reservationStatus;
     }
 
 }
