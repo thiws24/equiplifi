7468115e71dc6917007d5a3f262d782f54de514f
Marvin <kern.marvin@outlook.de>
2024-11-21 11:16:32 +0100

    Updated migration files

--- backend/inventoryservice/src/main/resources/db/migration/V1.0.0__inventoryservice.sql
+++ backend/inventoryservice/src/main/resources/db/migration/V1.0.0__inventoryservice.sql
@@ -3,7 +3,6 @@ CREATE SEQUENCE IF NOT EXISTS InventoryItem_SEQ START WITH 1 INCREMENT BY 50;
 CREATE TABLE InventoryItem (
     id BIGINT NOT NULL,
     name VARCHAR(255),
-    description VARCHAR(255),
     icon VARCHAR(255),
     photoUrl VARCHAR(255),
     urn VARCHAR(255),
