695fe6326fc66c46ef562a6542fab9e81a087bf9
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2024-11-25 23:15:07 +0100

    🧪 added failing integration test with testcontainers

---
+++ backend/mailservice/src/main/java/de/equipli/dto/inventoryservice/InventoryItemCreateDto.java
@@ -0,0 +1,58 @@
+package de.equipli.dto.inventoryservice;
+
+public class InventoryItemCreateDto {
+
+    //{
+    //  "name": "Volleyball",
+    //  "photoUrl": "https://example.com/photo.jpg",
+    //  "icon": "🏐",
+    //  "urn": "example-urn"
+    //}
+
+    private String name;
+    private String photoUrl;
+    private String icon;
+    private String urn;
+
+    public InventoryItemCreateDto(String name, String photoUrl, String icon, String urn) {
+        this.name = name;
+        this.photoUrl = photoUrl;
+        this.icon = icon;
+        this.urn = urn;
+    }
+
+    public InventoryItemCreateDto() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getUrn() {
+        return urn;
+    }
+
+    public void setUrn(String urn) {
+        this.urn = urn;
+    }
+}
--- backend/mailservice/src/test/java/de/equipli/endpoints/MailEndpointTest.java
+++ backend/mailservice/src/test/java/de/equipli/endpoints/MailEndpointTest.java
@@ -1,29 +1,119 @@
 package de.equipli.endpoints;
 
+import de.equipli.dto.inventoryservice.InventoryItemCreateDto;
+import de.equipli.dto.inventoryservice.InventoryItemDto;
+import de.equipli.dto.mail.CollectMailCreateDto;
 import io.quarkus.test.junit.QuarkusTest;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.Is.is;
 
 @QuarkusTest
+@Testcontainers
+@Disabled
 public class MailEndpointTest {
 
+    @ConfigProperty(name = "keycloak.image")
+    String keycloakImage;
+
+    @ConfigProperty(name = "inventory-service.image")
+    String inventoryServiceImage;
+
+    @ConfigProperty(name = "quarkus.rest-client.InventoryService.uri")
+    String inventoryServiceUri;
+
+    private static final Network sharedNetwork = Network.newNetwork();
+    static GenericContainer<?> inventoryService;
+    static GenericContainer<?> keycloak;
+    static PostgreSQLContainer<?> inventoryServiceDb;
+
+
+
+
+    @BeforeEach
+    void startContainers() {
+
+        inventoryServiceDb = new PostgreSQLContainer<>("postgres:17")
+                .withNetworkAliases("inventoryservicedb")
+                .withDatabaseName("inventoryservicedb")
+                .withUsername("inventoryservicedbuser")
+                .withPassword("inventoryservicedbpw")
+                .withNetwork(sharedNetwork)
+                ;
+        inventoryServiceDb.start();
+
+        keycloak = new GenericContainer(keycloakImage)
+                 .withNetworkAliases("keycloak")
+                .withNetwork(sharedNetwork);
+
+        inventoryService = new GenericContainer(inventoryServiceImage)
+                .withExposedPorts(8080)
+                .dependsOn(inventoryServiceDb)
+                .withEnv("QUARKUS_DATASOURCE_JDBC_URL", "jdbc:postgresql://inventoryservicedb:5432/inventoryservicedb")
+                .withEnv("QUARKUS_DATASOURCE_USERNAME", inventoryServiceDb.getUsername())
+                .withEnv("QUARKUS_DATASOURCE_PASSWORD", inventoryServiceDb.getPassword())
+                    .withNetworkAliases("inventoryservice")
+                .withNetwork(sharedNetwork);
+
+        keycloak.start();
+        inventoryService.start();
+
+        //Prepare inventory service
+
+        InventoryItemCreateDto inventoryItemCreateDto = new InventoryItemCreateDto();
+        inventoryItemCreateDto.setName("Volleyball");
+        inventoryItemCreateDto.setPhotoUrl("https://example.com/photo.jpg");
+        inventoryItemCreateDto.setIcon("🏐");
+        inventoryItemCreateDto.setUrn("example-urn");
+
+        // why is this not working?
+        // Idea: Testcontainer where the Test is running is not able to reach the inventory service
+        String inventoryServiceUriForPost = inventoryServiceUri + "/inventoryitems"; ;
+
+        given()
+                .contentType("application/json")
+                .body(inventoryItemCreateDto)
+                .when().post("http://localhost:8080/inventoryitems")
+                .then()
+                .statusCode(201)
+                .assertThat()
+                .body(is("Das InventoryItem wurde erfolgreich erstellt"));
+
+
+
+    }
 
     @Test
     void testSendCollectionMailEndPoint() {
 
+        CollectMailCreateDto collectMailCreateDto = new CollectMailCreateDto();
+        collectMailCreateDto.setUserId("df8e4444-4e04-4fd7-a9cd-8f938ea749c2"); //Id of Alice in Keycloak
+        collectMailCreateDto.setItemId("1");
+        collectMailCreateDto.setStartDate("24.12.2024");
+        collectMailCreateDto.setEndDate("24.01.2025");
 
-        /*MailDTO mailDTO = new MailDTO("test@test.de", "item", "2021-01-01", "2021-01-02", "pickupLocation", "pickupLocation");
 
         given()
                 .contentType("application/json")
-                .body(mailDTO)
+                .body(collectMailCreateDto)
                 .when().post("/sendCollectionMail")
                 .then()
                 .statusCode(200)
                 .assertThat()
-                .body(is("{\"result\":\"Collection reminder sent successfully\"}"));*/
+                .body(is("{\"result\":\"Collection reminder sent successfully\"}"));
 
     }
 }
