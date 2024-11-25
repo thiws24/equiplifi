2d3fcdebcb33ab75ef198c5948d8109e814ba347
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2024-11-25 21:14:23 +0100

    ✅ fixed CollectMailProcessorTest.java

--- backend/mailservice/src/test/java/de/equipli/processor/CollectMailProcessorTest.java
+++ backend/mailservice/src/test/java/de/equipli/processor/CollectMailProcessorTest.java
@@ -1,5 +1,6 @@
 package de.equipli.processor;
 
+import de.equipli.dto.inventoryservice.InventoryItemDto;
 import de.equipli.dto.mail.CollectMailCreateDto;
 import de.equipli.processors.mail.CollectMailProcessor;
 import io.quarkus.qute.Template;
@@ -19,7 +20,6 @@ import static org.mockito.Mockito.mock;
 @QuarkusTest
 public class CollectMailProcessorTest {
 
-    // Quarkus injiziert das Template
     @Inject
     Template collectmail;
 
@@ -29,15 +29,26 @@ public class CollectMailProcessorTest {
 
     @Test
     void process_processesTemplateCorrect() throws Exception {
+
+        // Arrange
         CollectMailCreateDto collectMailCreateDto = new CollectMailCreateDto();
-        collectMailCreateDto.setUserId("2d751263-0e51-4bac-864f-7a8ae80999dc"); //Id of Alice in Keycloak
-        collectMailCreateDto.setItemId("TestItem");
+        collectMailCreateDto.setUserId("df8e4444-4e04-4fd7-a9cd-8f938ea749c2"); //Id of Alice in Keycloak
+        collectMailCreateDto.setItemId("1");
         collectMailCreateDto.setStartDate("24.12.2024");
         collectMailCreateDto.setEndDate("24.01.2025");
 
-
         Exchange exchange = new DefaultExchange(new DefaultCamelContext());
         exchange.getIn().setBody(collectMailCreateDto);
+
+        // mock call to keycloak and inventoryservice
+        exchange.setProperty("nameOfUser", "TestName");
+
+        InventoryItemDto item = new InventoryItemDto();
+        item.setName("Volleyball");
+        item.setPhotoUrl("https://example.com/photo.jpg");
+        item.setIcon("🏐");
+        item.setUrn("example-urn");
+        exchange.setProperty("item", item);
 
         collectMailProcessor.process(exchange);
 
@@ -48,15 +59,11 @@ public class CollectMailProcessorTest {
         assert !generatedBody.matches(".*\\{.*?\\}.*") :
                 "Template contains unresolved variables: " + generatedBody;
 
-        // check for receiver
-        assertEquals("test@test.de", exchange.getProperty("to"));
-
         // check for content
         assertTrue(generatedBody.contains("TestName"));
-        assertTrue(generatedBody.contains("TestItem"));
+        assertTrue(generatedBody.contains("Volleyball 🏐"));
         assertTrue(generatedBody.contains("24.12.2024"));
         assertTrue(generatedBody.contains("24.01.2025"));
-        assertTrue(generatedBody.contains("TestLocation"));
 
     }
 
