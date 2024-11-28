7bbcacb166c5777f44b4688f54d154a04f18b916
janniknu <jannik.nuesslein@gmx.de>
2024-11-28 17:51:01 +0100

    adjusted QR-Code Generation + removed Paths for volume

--- backend/qrservice/src/main/java/de/equipli/QRGeneratorResource.java
+++ backend/qrservice/src/main/java/de/equipli/QRGeneratorResource.java
@@ -10,19 +10,18 @@ import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.graphics.image.LosslessFactory;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 @ApplicationScoped
 @Path("/qr")
 public class QRGeneratorResource {
     // Define 800 dpi and 62mm label size
-    private static final float DPI = 800;
+    private static final float DPI = 1400;
     private static final float labelSizeInMM = 62.0f;
 
     @GET
@@ -75,48 +74,36 @@ public class QRGeneratorResource {
     private BufferedImage generateQrCodeImage(String urn, String text) throws IOException {
         // Define End-File size in pixels
         int imageSizeInPixels = (int) (labelSizeInMM / 25.4f * DPI);
-        // Define QR-Code size in pixels at 70% of the total image size
-        int qrSize = (int)(imageSizeInPixels * 0.7);
+        // Define QR-Code size in pixels at % of the total image size
+        int qrSize = (int)(imageSizeInPixels * 0.9);
+        // Define Font-Size
+        int fontSize = imageSizeInPixels / 10;
 
         try {
             ByteArrayOutputStream qrStream = QRCode.from(urn).withSize(qrSize, qrSize).stream();
             BufferedImage qrImage = ImageIO.read(new ByteArrayInputStream(qrStream.toByteArray()));
 
-            int qrWidth = qrImage.getWidth();
-            int qrHeight = qrImage.getHeight();
-            System.out.println("QR-Code Size: " + qrSize + "QR-Code Width: " + qrWidth + " QR-Code Height: " + qrHeight);
-
-            Graphics2D g = qrImage.createGraphics();
-            g.setFont(new Font("Arial", Font.PLAIN, imageSizeInPixels / 20)); // Font size depending on image size
-            FontMetrics metrics = g.getFontMetrics();
-            int textHeight = metrics.getHeight(); // Height of the text in pixels
-            g.dispose();
-
-            int totalHeight = qrHeight + textHeight;
-            int imageWidth = imageSizeInPixels;
-            int imageHeight = totalHeight;
-            System.out.println("Image Width: " + imageWidth + " Image Height: " + imageHeight);
-
-            BufferedImage combinedImage = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_RGB);
-            g = combinedImage.createGraphics();
+            BufferedImage combinedImage = new BufferedImage(imageSizeInPixels, imageSizeInPixels, BufferedImage.TYPE_INT_RGB);
+            Graphics2D g = combinedImage.createGraphics();
 
             // set the background
             g.setColor(Color.WHITE);
-            g.fillRect(0, 0, imageWidth, imageHeight);
+            g.fillRect(0, 0, imageSizeInPixels, imageSizeInPixels);
 
             // draw QR-Code
-            int qrX = (imageWidth - qrWidth) / 2; // center QR-Code
+            int qrX = (imageSizeInPixels - qrSize) / 2; // center QR-Code
             int qrY = 0; // QR-Code on top
             g.drawImage(qrImage, qrX, qrY, null);
 
             // write Text
-            g.setFont(new Font("Arial", Font.PLAIN, imageSizeInPixels / 20)); // Font size depending on image size
+            g.setFont(new Font("Arial", Font.PLAIN, fontSize)); // Font size depending on image size
             g.setColor(Color.BLACK);
-            int textWidth = metrics.stringWidth(text);
-            int textX = (imageWidth - textWidth) / 2; // center Text
-            int textY = qrHeight + (textHeight / 2); // center Text
-            g.drawString(text, textX, textY);
-
+            int textWidth = g.getFontMetrics().stringWidth(text);
+            int textX = (imageSizeInPixels - textWidth) / 2; // center Text
+            if (textWidth>imageSizeInPixels){ // Text to long for Label --> do not center it to see Text from beginning
+                textX = 0;
+            }
+            g.drawString(text, textX, qrSize);
             g.dispose();
             return combinedImage;
 
@@ -129,36 +116,24 @@ public class QRGeneratorResource {
     private byte[] writePng(BufferedImage image) throws IOException {
         ByteArrayOutputStream pngStream = new ByteArrayOutputStream();
         javax.imageio.ImageIO.write(image, "png", pngStream);
-        // noch zu löschen
-        Files.write(Paths.get("src/main/resources/qrCodes/qr.png"), pngStream.toByteArray());
         return pngStream.toByteArray();
     }
 
     //Create PDF as a byte array
     private byte[] writePdf(BufferedImage image) throws IOException {
         PDDocument document = new PDDocument();
-        float widthInPoints = labelSizeInMM / 25.4f * DPI * 72;
-        float heightInPoints = widthInPoints; // Square PDF
-        PDPage page = new PDPage(new PDRectangle(widthInPoints, heightInPoints));
+        float sizeInPoints = labelSizeInMM * 2.835f;
+        PDPage page = new PDPage(new PDRectangle(sizeInPoints, sizeInPoints));
         document.addPage(page);
 
         PDPageContentStream contentStream = new PDPageContentStream(document, page);
-        contentStream.drawImage(
-                //Lossless image
-                LosslessFactory.createFromImage(document, image),
-                0,
-                0,
-                widthInPoints,
-                heightInPoints
-        );
+        PDImageXObject pdImage = LosslessFactory.createFromImage(document, image);
 
+        contentStream.drawImage(pdImage, 0, 0, sizeInPoints, sizeInPoints);
         contentStream.close();
         ByteArrayOutputStream pdfStream = new ByteArrayOutputStream();
         document.save(pdfStream);
         document.close();
-
-        // noch zu löschen
-        Files.write(Paths.get("src/main/resources/qrCodes/qr.pdf"), pdfStream.toByteArray());
         return pdfStream.toByteArray();
     }
 
