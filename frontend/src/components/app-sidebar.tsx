f767ac8af0137b3a28efd7b682c811a45a54f0ad
RR <raluca97rr@gmail.com>
2024-12-01 21:25:18 +0100

    Remove SidebarProvider from AppSidebar

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -1,4 +1,4 @@
-import {Calendar, ChevronUp, Home, Inbox, Search, Settings, User2} from "lucide-react"
+import { ChevronUp, Home, Inbox, User2} from "lucide-react"
 import {
     SidebarContent, SidebarFooter,
     SidebarGroup,
@@ -37,80 +37,78 @@ export function AppSidebar() {
     const { state } = useSidebar();
     return (
         (
-            <SidebarProvider>
-                <Sidebar collapsible='icon'>
-                    <SidebarHeader><img src="/equipli-logo.svg"
-                                        className="w-12 h-auto"
-                                        alt="equipli logo"/></SidebarHeader>
-                    <SidebarContent>
-                        <SidebarGroup>
-                            <SidebarGroupLabel className='text-xl text-customBlue'>Allgemein</SidebarGroupLabel>
-                            <SidebarGroupContent>
-                                <SidebarMenu>
-                                    {itemsAllgemein.map((item) => (
-                                        <SidebarMenuItem key={item.title}>
-                                            <SidebarMenuButton asChild
-                                                               className="bg-customBeige text-customOrange hover:bg-customRed hover:text-customBeige">
-                                                <a href={item.url} className="flex items-center gap-2">
-                                                    {item.icon && <item.icon/>}
-                                                    <span>{item.title}</span>
-                                                </a>
-                                            </SidebarMenuButton>
-                                        </SidebarMenuItem>
-                                    ))}
-                                </SidebarMenu>
-                            </SidebarGroupContent>
-                            <SidebarGroupLabel className='text-xl text-customBlue'>Lagerwart</SidebarGroupLabel>
-                            <SidebarGroupContent>
-                                <SidebarMenu>
-                                    {itemsLagerwart.map((item) => (
-                                        <SidebarMenuItem key={item.title}>
-                                            <SidebarMenuButton asChild
-                                                               className="bg-customBeige text-customOrange hover:bg-customRed hover:text-customBeige">
-                                                <a href={item.url} className="flex items-center gap-2">
-                                                    {item.icon && <item.icon/>}
-                                                    <span>{item.title}</span>
-                                                </a>
-                                            </SidebarMenuButton>
-                                        </SidebarMenuItem>
-                                    ))}
-                                </SidebarMenu>
-                            </SidebarGroupContent>
-                        </SidebarGroup>
-                    </SidebarContent>
+            <Sidebar collapsible='icon'>
+                <SidebarHeader>
+                    <img src="/equipli-logo.svg" className="w-12 h-auto" alt="equipli logo" />
+                </SidebarHeader>
+                <SidebarContent>
+                    <SidebarGroup>
+                        <SidebarGroupLabel className='text-xl text-customBlue'>Allgemein</SidebarGroupLabel>
+                        <SidebarGroupContent>
+                            <SidebarMenu>
+                                {itemsAllgemein.map((item) => (
+                                    <SidebarMenuItem key={item.title}>
+                                        <SidebarMenuButton asChild
+                                                           className="bg-customBeige text-customOrange hover:bg-customRed hover:text-customBeige">
+                                            <a href={item.url} className="flex items-center gap-2">
+                                                {item.icon && <item.icon/>}
+                                                <span>{item.title}</span>
+                                            </a>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                                ))}
+                            </SidebarMenu>
+                        </SidebarGroupContent>
+                        <SidebarGroupLabel className='text-xl text-customBlue'>Lagerwart</SidebarGroupLabel>
+                        <SidebarGroupContent>
+                            <SidebarMenu>
+                                {itemsLagerwart.map((item) => (
+                                    <SidebarMenuItem key={item.title}>
+                                        <SidebarMenuButton asChild
+                                                           className="bg-customBeige text-customOrange hover:bg-customRed hover:text-customBeige">
+                                            <a href={item.url} className="flex items-center gap-2">
+                                                {item.icon && <item.icon/>}
+                                                <span>{item.title}</span>
+                                            </a>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                                ))}
+                            </SidebarMenu>
+                        </SidebarGroupContent>
+                    </SidebarGroup>
+                </SidebarContent>
 
-                    {/* Footer der Sidebar */}
-                    <SidebarFooter>
-                        <SidebarMenu>
-                            <SidebarMenuItem>
-                                <DropdownMenu>
-                                    <DropdownMenuTrigger asChild>
-                                        <button className="flex items-center gap-2 text-customBlack">
-                                            {state === "collapsed" ? (
-                                                // Nur Icon anzeigen, wenn Sidebar collapsed ist
-                                                <User2 className="h-6 w-6"/>
-                                            ) : (
-                                                // Icon und Text anzeigen, wenn Sidebar expanded ist
-                                                <>
-                                                    <User2/> Abmelden
-                                                    <ChevronUp className="ml-auto"/>
-                                                </>
-                                            )}
-                                        </button>
-                                    </DropdownMenuTrigger>
-                                    <DropdownMenuContent
-                                        side="top"
-                                        className="bg-white shadow-lg rounded-md text-customBlack"
-                                    >
-                                        <DropdownMenuItem>
-                                            <span>Abmelden</span>
-                                        </DropdownMenuItem>
-                                    </DropdownMenuContent>
-                                </DropdownMenu>
-                            </SidebarMenuItem>
-                        </SidebarMenu>
-                    </SidebarFooter>
-                </Sidebar>
-            </SidebarProvider>
+                {/* Footer der Sidebar */}
+                <SidebarFooter>
+                    <SidebarMenu>
+                        <SidebarMenuItem>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger asChild>
+                                    <button className="flex items-center gap-2 text-customBlack">
+                                        {state === "collapsed" ? (
+                                            // Nur Icon anzeigen, wenn Sidebar collapsed ist
+                                            <User2 className="h-6 w-6"/>
+                                        ) : (
+                                            // Icon und Text anzeigen, wenn Sidebar expanded ist
+                                            <>
+                                                <User2/> Abmelden
+                                                <ChevronUp className="ml-auto"/>
+                                            </>
+                                        )}
+                                    </button>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent
+                                    side="top"
+                                    className="bg-white shadow-lg rounded-md text-customBlack"
+                                >
+                                    <DropdownMenuItem>
+                                        <span>Abmelden</span>
+                                    </DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        </SidebarMenuItem>
+                    </SidebarMenu>
+                </SidebarFooter>
+            </Sidebar>
         ));
 }
