aa389af21895b44cd345003bd1603e6dbbd5c8c9
HeiligeMeier <steffheili846@t-online.de>
2024-12-03 17:30:18 +0100

    Routing of new pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Detail from "./pages/Detail"
 import Lend from "./pages/Lend"
 import LendCategory from "./pages/LendCategory"
+import CategoryDetails from "./pages/CategoryDetails"
 import React from "react";
 import Home from "./pages/Home";
 
@@ -10,9 +11,10 @@ const App = () => {
       <div className='bg-customBeige'>
           <Routes>
               <Route path="/" element={<Home/>}/>
+              <Route path="/:id/create" element={<Home/>}/>
 
               <Route path="/item/:id" element={<Detail/>}/>
-              {/*<Route path="/category/:id" element={<DetailCategory/>}/>*/}
+              <Route path="/category/:id" element={<CategoryDetails/>}/>
 
               <Route path="/item/:id/reservation" element={<Lend/>}/>
               <Route path="/category/:id/reservation" element={<LendCategory/>}/>
