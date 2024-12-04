bae03dc0a4a5f2c9dc7d2e31cd327d8dd6ddadd4
Marvin <kern.marvin@outlook.de>
2024-12-04 01:18:10 +0100

    Updated inventoryservice tests

--- backend/inventoryservice/src/test/java/de/equipli/inventory/InventoryItemResourceTest.java
+++ backend/inventoryservice/src/test/java/de/equipli/inventory/InventoryItemResourceTest.java
@@ -1,9 +1,14 @@
 package de.equipli.inventory;
 
+import de.equipli.inventory.jpa.Category;
 import de.equipli.inventory.jpa.InventoryItem;
+import de.equipli.inventory.rest.dto.UpdateCategoryRequest;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -12,99 +17,265 @@ import static org.hamcrest.CoreMatchers.is;
 class InventoryItemResourceTest {
 
     @Test
-    void testAddInventoryItem() {
-        String name = "Test Item";
-        String description = "This is a test item";
+    void testAddCategory() {
+        String name = "Test Category";
+        String description = "This is a test category";
         String icon = "icon";
-        String urn = "urn:test:item";
         String photoUrl = "https://www.example.com/photo.jpg";
-        String itemStatus = "damaged";
 
-        InventoryItem item = new InventoryItem();
-        /*item.setName(name);
-        item.setDescription(description);
-        item.setIcon(icon);
-        //item.setUrn(urn);
-        item.setPhotoUrl(photoUrl);
-        item.setItemStatus(itemStatus);*/
+        Category category = new Category();
+        category.setName(name);
+        category.setDescription(description);
+        category.setIcon(icon);
+        category.setPhotoUrl(photoUrl);
 
         given()
                 .contentType(ContentType.JSON)
-                .body(item)
+                .body(category)
                 .when()
-                .post("/inventoryitems")
+                .post("/categories")
                 .then()
                 .statusCode(201)
                 .body("name", is(name))
                 .body("description", is(description))
-                .body("icon", is(icon))
-                //.body("urn", is(urn))
                 .body("photoUrl", is(photoUrl))
-                .body("itemStatus", is(itemStatus));
+                .body("icon", is(icon));
     }
 
     @Test
-    void testGetInventoryItems() {
+    void testGetCategories() {
         given()
                 .when()
-                .get("/inventoryitems")
+                .get("/categories")
                 .then()
                 .statusCode(200);
     }
 
     @Test
-    void testPutInventoryItems() {
-        InventoryItem item = new InventoryItem();
-        /*item.setName("Test Item");*/
+    void testUpdateCategory() {
+        Category category = new Category();
+        category.setName("Test Category");
+        category.setDescription("This is a test category");
+        category.setIcon("icon");
+        category.setPhotoUrl("https://www.example.com/photo.jpg");
 
         int id = given()
                 .contentType(ContentType.JSON)
-                .body(item)
+                .body(category)
                 .when()
-                .post("/inventoryitems")
+                .post("/categories")
                 .then()
                 .statusCode(201)
                 .extract().path("id");
 
-        InventoryItem updatedItem = new InventoryItem();
-        /*updatedItem.setName("Updated Test Item");*/
+        UpdateCategoryRequest updatedCategory = new UpdateCategoryRequest();
+
+        updatedCategory.setName("Updated Category");
+        updatedCategory.setDescription("Updated Description");
+        updatedCategory.setIcon("updated-icon");
+        updatedCategory.setPhotoUrl("https://www.example.com/updated-photo.jpg");
 
         given()
                 .contentType(ContentType.JSON)
-                .body(updatedItem)
+                .body(updatedCategory)
                 .when()
-                .put("/inventoryitems/" + id)
+                .put("/categories/" + id)
                 .then()
                 .statusCode(200)
-                .body("name", is("Updated Test Item"));
+                .body("name", is("Updated Category"));
     }
 
     @Test
-    void testDeleteInventoryItem() {
-        InventoryItem item = new InventoryItem();
-        /*item.setName("Test Item");*/
+    void testDeleteCategory() {
+        Category category = new Category();
+        category.setName("Test Category");
+
+        List<InventoryItem> items = new ArrayList<>();
+        items.add(new InventoryItem());
+        category.setItems(items);
 
         int id = given()
                 .contentType(ContentType.JSON)
-                .body(item)
+                .body(category)
                 .when()
-                .post("/inventoryitems")
+                .post("/categories")
                 .then()
                 .statusCode(201)
                 .extract().path("id");
 
-        // Item löschen
+        int itemId = given()
+                .contentType(ContentType.JSON)
+                .body(new InventoryItem())
+                .when()
+                .post("/categories/" + id + "/items")
+                .then()
+                .statusCode(201)
+                .extract().path("id");
+
+        // Kategorie löschen
         given()
                 .when()
-                .delete("/inventoryitems/" + id)
+                .delete("/categories/" + id)
                 .then()
                 .statusCode(204);
+
+        // Checken, ob Kategorie gelöscht wurde
+        given()
+                .when()
+                .get("/categories/" + id)
+                .then()
+                .statusCode(404);
 
         // Checken, ob Item gelöscht wurde
         given()
                 .when()
-                .get("/inventoryitems/" + id)
+                .get("/items/" + itemId)
+                .then()
+                .statusCode(404);
+
+    }
+
+    @Test
+    void testAddInventoryItem() {
+        Category category = new Category();
+        category.setName("Test Category");
+
+        int categoryId = given()
+                .contentType(ContentType.JSON)
+                .body(category)
+                .when()
+                .post("/categories")
+                .then()
+                .statusCode(201)
+                .extract().path("id");
+
+        InventoryItem item = new InventoryItem();
+        item.setLocation("Test Location");
+
+        int id = given()
+                .contentType(ContentType.JSON)
+                .body(item)
+                .when()
+                .post("/categories/" + categoryId + "/items")
+                .then()
+                .statusCode(201)
+                .extract().path("id");
+
+        given()
+                .when()
+                .get("/categories/" + categoryId + "/items/" + id)
+                .then()
+                .statusCode(200)
+                .body("location", is("Test Location"));
+    }
+
+    @Test
+    void testGetInventoryItems() {
+        Category category = new Category();
+        category.setName("Test Category");
+
+        int categoryId = given()
+                .contentType(ContentType.JSON)
+                .body(category)
+                .when()
+                .post("/categories")
+                .then()
+                .statusCode(201)
+                .extract().path("id");
+
+        InventoryItem item = new InventoryItem();
+        item.setLocation("Test Location");
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(item)
+                .when()
+                .post("/categories/" + categoryId + "/items")
+                .then()
+                .statusCode(201);
+
+        given()
+                .when()
+                .get("/categories/" + categoryId + "/items")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void testUpdateInventoryItem() {
+        Category category = new Category();
+        category.setName("Test Category");
+
+        int categoryId = given()
+                .contentType(ContentType.JSON)
+                .body(category)
+                .when()
+                .post("/categories")
+                .then()
+                .statusCode(201)
+                .extract().path("id");
+
+        InventoryItem item = new InventoryItem();
+        item.setLocation("Test Location");
+
+        int id = given()
+                .contentType(ContentType.JSON)
+                .body(item)
+                .when()
+                .post("/categories/" + categoryId + "/items")
+                .then()
+                .statusCode(201)
+                .extract().path("id");
+
+        item.setLocation("Updated Location");
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(item)
+                .when()
+                .put("/categories/" + categoryId + "/items/" + id)
+                .then()
+                .statusCode(200)
+                .body("location", is("Updated Location"));
+    }
+
+    @Test
+    void testDeleteInventoryItem() {
+        Category category = new Category();
+        category.setName("Test Category");
+
+        int categoryId = given()
+                .contentType(ContentType.JSON)
+                .body(category)
+                .when()
+                .post("/categories")
+                .then()
+                .statusCode(201)
+                .extract().path("id");
+
+        InventoryItem item = new InventoryItem();
+        item.setLocation("Test Location");
+
+        int id = given()
+                .contentType(ContentType.JSON)
+                .body(item)
+                .when()
+                .post("/categories/" + categoryId + "/items")
+                .then()
+                .statusCode(201)
+                .extract().path("id");
+
+        given()
+                .when()
+                .delete("/categories/" + categoryId + "/items/" + id)
+                .then()
+                .statusCode(204);
+
+        given()
+                .when()
+                .get("/categories/" + categoryId + "/items/" + id)
                 .then()
                 .statusCode(404);
     }
+
 }
