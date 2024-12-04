6dea06473673a523817e8cb224601e3aada9194c
Marvin <kern.marvin@outlook.de>
2024-12-04 01:17:29 +0100

    Added DTOs to create & update categories

---
+++ backend/inventoryservice/src/main/java/de/equipli/inventory/rest/dto/UpdateCategoryRequest.java
@@ -0,0 +1,45 @@
+package de.equipli.inventory.rest.dto;
+
+public class UpdateCategoryRequest {
+
+    private String name;
+    private String description;
+    private String icon;
+    private String photoUrl;
+
+    public UpdateCategoryRequest() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+}
