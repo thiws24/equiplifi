c78a7acc650ce823c7b4a856673893f916d2892d
Marvin <kern.marvin@outlook.de>
2024-12-06 18:09:14 +0100

    Changed repository to get category by item id

--- backend/inventoryservice/src/main/java/de/equipli/inventory/jpa/CategoryRepository.java
+++ backend/inventoryservice/src/main/java/de/equipli/inventory/jpa/CategoryRepository.java
@@ -5,4 +5,9 @@ import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class CategoryRepository implements PanacheRepository<Category> {
+
+    public Category findByItemId(Long itemId) {
+        return find("SELECT c FROM Category c JOIN c.items i WHERE i.id = ?1", itemId).firstResult();
+    }
+
 }
--- backend/inventoryservice/src/main/java/de/equipli/inventory/jpa/InventoryRepository.java
+++ backend/inventoryservice/src/main/java/de/equipli/inventory/jpa/InventoryRepository.java
@@ -6,8 +6,4 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class InventoryRepository implements PanacheRepository<InventoryItem> {
 
-    public Category findCategoryByItemId(Long itemId) {
-        return find("id", itemId).firstResult().getCategory();
-    }
-
 }
