14af3c12c3441395aee947079990130114d50cd1
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2024-12-08 22:02:28 +0100

    🚀 implemented Anfragebestätigung, Lagerwartbestätigung & Benachrichtigung über Ablehnung der Anfrage

--- backend/mailservice/src/main/java/de/equipli/processors/mail/MailProcessor.java
+++ backend/mailservice/src/main/java/de/equipli/processors/mail/MailProcessor.java
@@ -5,24 +5,28 @@ import de.equipli.dto.mail.MailCreateDto;
 import de.equipli.dto.mail.MailDto;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
+import io.quarkus.qute.runtime.TemplateProducer;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
-import java.util.ArrayList;
+
 import java.util.List;
+
 
 @ApplicationScoped
 public class MailProcessor implements Processor {
 
     @Inject
-    Template mail;
+    TemplateProducer templateProducer;
 
     @Override
     public void process(Exchange exchange) throws Exception {
 
         List<MailDto> mailDtoList = exchange.getMessage().getBody(List.class);
+
+        String mailTempalte = exchange.getProperty("mailTemplate", String.class);
 
 
         // The First one is also the only one as checked in ValidationProcessor
@@ -31,7 +35,7 @@ public class MailProcessor implements Processor {
 
 
         // Render the template with Qute
-        TemplateInstance templateInstance = mail
+        TemplateInstance templateInstance = templateProducer.getInjectableTemplate(mailTempalte)
                 .data("name", nameOfUser)
                 .data("mailDtoList", mailDtoList);
 
@@ -39,9 +43,9 @@ public class MailProcessor implements Processor {
 
         // create mail
         //TODO: Make Subject dynamic
-        exchange.getIn().setHeader("Subject", "Abholerinnerung | Equipli");
+        exchange.getIn().setHeader("Subject", "Eine Nachricht von Equipli.de!");
         exchange.getIn().setHeader("To", receiverMail);
-        exchange.getIn().setHeader("From", "info@equipli.de");
+        exchange.getIn().setHeader("From", "info.equipli@gmail.com");
         exchange.getIn().setHeader("Content-Type", "text/html");
         exchange.getIn().setBody(htmlTemplate);
     }
