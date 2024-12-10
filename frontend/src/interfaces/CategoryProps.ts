4cb0d642c3c3c05f4b057aca0c104fe13ea3d507
HeiligeMeier <steffheili846@t-online.de>
2024-12-10 13:03:44 +0100

    correction of interface

--- frontend/src/interfaces/CategoryProps.ts
+++ frontend/src/interfaces/CategoryProps.ts
@@ -1,10 +1,17 @@
+interface ItemProps {
+    id: number,
+    status: string,
+    location: string,
+    urn: string
+}
+
 export interface CategoryProps {
     id: number
     name: string
     description?: string
     icon: string
     photoUrl: string
-    items: [id: number, status: string, location: string, urn: string]
+    items: ItemProps[]
 }
 
 // itemStatus Enum: [ OK, BROKEN, IN_MAINTENANCE, MAINTENANCE_REQUIRED, LOST ]
