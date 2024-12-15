d40ed56232184c41a831b5e4c2a7c2634bd3085d
HeiligeMeier <steffheili846@t-online.de>
2024-12-15 16:21:16 +0100

    Page Not Found implemented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,23 +7,24 @@ import CategoryDetails from "./pages/CategoryDetails"
 import React from "react"
 import Home from "./pages/Home"
 import Reservations from "./pages/Reservations"
+import NoPage from "./pages/NoPage"
 
 const App = () => {
     return (
         <div className="bg-customBeige">
             <Routes>
                 <Route path="/" element={<Home />} />
-
                 <Route path="/item/:id" element={<Detail />} />
                 <Route path="/category/:id" element={<CategoryDetails />} />
                 <Route path="/category/create" element={<CategoryCreation />} />
-
                 <Route path="/item/:id/reservation" element={<Lend />} />
                 <Route
                     path="/category/:id/reservation"
                     element={<LendCategory />}
                 />
                 <Route path="/reservations" element={<Reservations />} />
+
+                <Route path="*" element={<NoPage />} />
             </Routes>
         </div>
     )
