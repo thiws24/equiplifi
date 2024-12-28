025aba37684465cee127d4b98556d2e38e7d1394
HeiligeMeier <steffheili846@t-online.de>
2024-12-28 12:00:16 +0100

    fixed Toast Colors

--- frontend/src/components/CustomToasts.tsx
+++ frontend/src/components/CustomToasts.tsx
@@ -14,38 +14,34 @@ export const CustomToasts = {
     success: ({ message, onClose, duration }: ToastProps) => {
         toast.success(
             <div style={{ display: "flex", alignItems: "center" }}>
-                <CheckCircle color="#5588c7" size={24} style={{ marginRight: "10px" }} />
+                <CheckCircle color="green" size={24} style={{ marginRight: "10px" }} />
                 <span className="mr-1 ml-1">{message}</span>
             </div>,
             {
-                className: "custom-toast",
+                className: "custom-toast success-toast",
                 pauseOnFocusLoss: false,
                 position: "top-center",
-                style: {
-                    backgroundColor: "#5588c7"
-                },
                 icon: false,
                 onClose: onClose || undefined,
-                autoClose: duration || 5000
+                autoClose: duration || 5000,
+                style: { color: "#333", fontFamily: "sans-serif, Arial" }
             }
         )
     },
     error: ({ message, onClose, duration }: ToastProps) => {
         toast.error(
             <div style={{ display: "flex", alignItems: "center" }}>
-                <FaExclamationCircle color="red" size={32} style={{ marginRight: "10px" }} />
+                <FaExclamationCircle color="red" size={24} style={{ marginRight: "10px" }} />
                 <span className="mr-1 ml-1">{message}</span>
             </div>,
             {
                 className: "custom-toast",
                 pauseOnFocusLoss: false,
                 position: "top-center",
-                style: {
-                    backgroundColor: "red"
-                },
                 icon: false,
                 onClose: onClose || undefined,
-                autoClose: duration || 5000
+                autoClose: duration || 5000,
+                style: { color: "#333", fontFamily: "sans-serif, Arial" }
             }
         )
     }
