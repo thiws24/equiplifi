98d93582267cc3b3522394782e73d78a26f558ef
HeiligeMeier <steffheili846@t-online.de>
2024-12-28 12:00:30 +0100

    added decline Reservation Button

--- frontend/src/components/ConfirmReservationCard.tsx
+++ frontend/src/components/ConfirmReservationCard.tsx
@@ -29,12 +29,13 @@ export const ConfirmReservationCard: React.FC<Props> = ({
     }
 
     return (
-        <div className="text-sm border p-5 rounded shadow-md space-y-5">
-            <div className="grid grid-cols-[max-content_auto] gap-x-10 gap-y-2">
+        <div
+            className="mb-10 text-sm border p-5 rounded shadow-md flex flex-col md:flex-row justify-start items-start md:items-center">
+            <div className="flex-1 min-w-[200px] grid grid-cols-[max-content_auto] gap-x-10 gap-y-2">
                 <b>Prozess-ID:</b>
                 <div>{processId}</div>
                 <b>Benutzername:</b>
-                <div>{data? data[0]?.userName : "-"}</div>
+                <div>{data ? data[0]?.userName : "-"}</div>
                 <b>Kategorie-ID:</b>
                 <div>{data ? data[0]?.categoryId : "-"}</div>
                 <b>Anzahl:</b>
@@ -43,23 +44,25 @@ export const ConfirmReservationCard: React.FC<Props> = ({
                 <div>{data ? formatDate(data[0]?.startDate) : "-"}</div>
                 <b>Enddatum:</b>
                 <div>{data ? formatDate(data[0]?.endDate) : "-"}</div>
-                <b>Item IDs:</b>
+                <b className="mb-4 md:mb-0">Item IDs:</b>
                 <div>{itemIds.join(", ")}</div>
             </div>
-            <div className="flex flex-col gap-2 mt-4 min-w-[200px]">
+            <div
+                className="flex flex-col gap-2 justify-center items-start md:justify-center md:items-center">
                 <button
-                    className="bg-customBlue text-customBeige text-sm px-4 py-2 rounded hover:brightness-90"
+                    className="w-48 bg-customBlue text-customBeige text-sm px-4 py-2 rounded hover:brightness-90"
                     onClick={handleConfirmation}
                 >
                     Reservierung bestätigen
                 </button>
                 <button
-                    className="bg-customRed text-customBeige text-sm px-4 py-2 rounded hover:brightness-90"
+                    className="w-48 bg-customRed text-customBeige text-sm px-4 py-2 rounded hover:brightness-90"
                     onClick={handleCancelation}
                 >
                     Reservierung ablehnen
                 </button>
             </div>
         </div>
+
     )
 }
