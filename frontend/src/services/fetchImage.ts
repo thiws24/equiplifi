8476de86253f818c2ead868c3effed830bd7480b
Marvin <kern.marvin@outlook.de>
2025-01-06 22:02:15 +0100

    Fetching images as image/jpeg instead of base64

--- frontend/src/services/fetchImage.ts
+++ frontend/src/services/fetchImage.ts
@@ -1,22 +1,28 @@
 export async function fetchImage(categoryId: number, token: string): Promise<string> {
-    let base64 = ""
+    let imageUrl = ""
     try {
         const res = await fetch(
             `${import.meta.env.VITE_INVENTORY_SERVICE_HOST}/categories/${categoryId}/image`,
             {
                 method: "GET",
                 headers: {
-                    Authorization: `Bearer ${token}`
+                    Authorization: `Bearer ${token}`,
+                    Accept: "image/jpeg"
                 }
             }
         )
 
         if (res.ok) {
-            base64 = "data:image/jpeg;base64," + await res.text()
+            const blob = await res.blob()
+            imageUrl = URL.createObjectURL(blob)
+        } else {
+            console.error("Error fetching image. Status:", res.status)
+            imageUrl = "/image-placeholder.jpg"
         }
     } catch (e) {
         console.error('Error fetching image', e)
+        imageUrl = "/image-placeholder.jpg"
     }
 
-    return base64
+    return imageUrl
 }
